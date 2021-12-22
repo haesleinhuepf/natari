e5ed762b71ebb26779af637fe11b267e6a0b9df0
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-12-22 14:44:05 +0100

    replaced global variables with a singleton implementation

--- natari/sliding_puzzle.py
+++ natari/sliding_puzzle.py
@@ -1,148 +1,162 @@
 import napari
 from napari_tools_menu import register_action
 
-# game config
-patch_size = 100
-
-current_pos_x = 0
-current_pos_y = 0
-
-game_state = 0
-game_chain = []
-
 
 @register_action(menu="Games > Sliding Puzzle")
-def sliding_puzzle(viewer : napari.Viewer):
-    # global variables are not good.
-    # Only in cheap game programming it's ok.
-    global game_chain
-    global game_state
+def sliding_puzzle(viewer: napari.Viewer):
+    game = SlidingPuzzleGame.instance(viewer)
+    game.start()
 
-    from napari._qt.qthreading import thread_worker
-    from skimage.io import imread, imshow
-    from pathlib import Path
-    import time
 
-    if len(viewer.layers) == 0:
-        data_path = Path(__file__).parent / "data"
-        dataset = imread(data_path / '17157718_1475080609170139_6436185275063838511_o.jpg')
-        viewer.add_image(dataset[100:1000,400:1600].copy())
+class SlidingPuzzleGame():
+    def __init__(self, viewer: napari.Viewer):
+        from napari._qt.qthreading import thread_worker
+        import time
+        self.game_layer = None
+        self.pos_x = 0
+        self.pos_y = 0
+        self.height = 0
+        self.width = 0
+        self.viewer = viewer
+        self.image = None
 
-    # start game
-    image = list(viewer.layers.selection)[0].data
-    draw_grid(image, patch_size)
-    image = crop_image(image, patch_size).copy()
-    width = image.shape[1]
-    height = image.shape[0]
+        def update_layers(data):
+            if self.game_layer is not None:
+                self.game_layer.data = data
 
-    game_layer = viewer.add_image(image)
+        @thread_worker
+        def loop_run():
+            while True:  # endless loop
+                data = self.game_loop()
+                yield data
+                time.sleep(0.05)
 
-    start_x = int(width / 2 / patch_size)
-    start_y = int(height / 2 / patch_size)
-    set_tile_to_zero(image, start_x, start_y, patch_size)
+        # Start the loop
+        worker = loop_run()
+        worker.yielded.connect(update_layers)
+        worker.start()
 
-    length = 50
-    game_chain = make_random_game(start_x, start_y, image, patch_size, length)
-    game_state = 0
+        # Key bindings for the game
+        @viewer.bind_key('w', overwrite=True)
+        def player_up_event(viewer):
+            if self.pos_y > 0:
+                self.game_chain.append('w')
 
-    def update_layers(data):
-        game_layer.data = data
+        @viewer.bind_key('a', overwrite=True)
+        def player_left_event(viewer):
+            if self.pos_x > 0:
+                self.game_chain.append('a')
 
-    @thread_worker
-    def loop_run(data, pos_x, pos_y):
-        global current_pos_y
-        global current_pos_x
-        while True:  # endless loop
-            data, pos_x, pos_y = game_loop(data, pos_x, pos_y)
-            current_pos_x = pos_x
-            current_pos_y = pos_y
-            yield data
-            time.sleep(0.05)
+        @viewer.bind_key('s', overwrite=True)
+        def player_down_event(viewer):
+            if self.pos_y < (self.height / self.patch_size) - 1:
+                self.game_chain.append('s')
 
-    # Start the loop
-    worker = loop_run(image, start_x, start_y)
-    worker.yielded.connect(update_layers)
-    worker.start()
+        @viewer.bind_key('d', overwrite=True)
+        def player_right_event(viewer):
+            if self.pos_x < (self.width / self.patch_size) - 1:
+                self.game_chain.append('d')
 
-    # Key bindings for the game
-    @viewer.bind_key('w', overwrite=True)
-    def player_up_event(viewer):
-        global current_pos_y
-        if current_pos_y > 0:
-            game_chain.append('w')
+        @viewer.bind_key('r', overwrite=True)
+        def player_random_next_step(viewer):
+            self.game_chain = self.game_chain + make_random_game(self.pos_x,self.pos_y,self.image,self.patch_size,1)
 
-    @viewer.bind_key('a', overwrite=True)
-    def player_left_event(viewer):
-        global current_pos_x
-        if current_pos_x > 0:
-            game_chain.append('a')
+        @viewer.bind_key('f', overwrite=True)
+        def player_find_home(viewer):
+            copy = self.game_chain.copy()
+            copy.reverse()
+            list_replace(copy, 'w', 't')
+            list_replace(copy, 's', 'w')
+            list_replace(copy, 't', 's')
+            list_replace(copy, 'a', 't')
+            list_replace(copy, 'd', 'a')
+            list_replace(copy, 't', 'd')
+            self.game_chain = self.game_chain + copy
 
-    @viewer.bind_key('s', overwrite=True)
-    def player_down_event(viewer):
-        global current_pos_y
-        if current_pos_y < (height / patch_size) - 1:
-            game_chain.append('s')
+    @classmethod
+    def instance(cls, viewer):
+        if not hasattr(cls, "_instance"):
+            cls._instance = SlidingPuzzleGame(viewer)
+        return cls._instance
 
-    @viewer.bind_key('d', overwrite=True)
-    def player_right_event(viewer):
-        global current_pos_x
-        if current_pos_x < (width / patch_size) - 1:
-            game_chain.append('d')
+    def start(self):
+        # game config
+        self.patch_size = 100
 
-    @viewer.bind_key('r', overwrite=True)
-    def player_random_next_step(viewer):
-        global game_chain
-        game_chain = game_chain + make_random_game(current_pos_x,current_pos_y,image,patch_size,1)
+        self.pos_x = 0
+        self.pos_y = 0
 
-    @viewer.bind_key('f', overwrite=True)
-    def player_find_home(viewer):
-        global game_chain
-        copy = game_chain
-        copy.reverse()
-        list_replace(copy, 'w', 't')
-        list_replace(copy, 's', 'w')
-        list_replace(copy, 't', 's')
-        list_replace(copy, 'a', 't')
-        list_replace(copy, 'd', 'a')
-        list_replace(copy, 't', 'd')
-        game_chain = game_chain + copy
+        self.game_state = 0
+        self.game_chain = []
+
+
+
+        from skimage.io import imread, imshow
+        from pathlib import Path
+
+        if len(self.viewer.layers) == 0:
+            data_path = Path(__file__).parent / "data"
+            dataset = imread(data_path / '17157718_1475080609170139_6436185275063838511_o.jpg')
+            self.viewer.add_image(dataset[100:1000,400:1600].copy())
+
+        # start game
+        self.image = list(self.viewer.layers.selection)[0].data
+        self.image = crop_image(self.image, self.patch_size).copy()
+        draw_grid(self.image, self.patch_size)
+        self.width = self.image.shape[1]
+        self.height = self.image.shape[0]
+
+        self.game_layer = self.viewer.add_image(self.image)
+
+        start_x = int(self.width / 2 / self.patch_size)
+        start_y = int(self.height / 2 / self.patch_size)
+        set_tile_to_zero(self.image, start_x, start_y, self.patch_size)
+
+        length = 50
+        self.game_chain = make_random_game(start_x, start_y, self.image, self.patch_size, length)
+        self.game_state = 0
+
+        self.pos_x = start_x
+        self.pos_y = start_y
+
+
+    def game_loop(self):
+
+        if self.game_state < len(self.game_chain):
+            if self.game_state < 0:
+                direction = self.game_chain[-1]
+            else:
+                direction = self.game_chain[self.game_state]
+
+            print(direction)
+
+            former_pos_x = self.pos_x
+            former_pos_y = self.pos_y
+
+            self.pos_x, self.pos_y = new_pos(self.pos_x, self.pos_y, direction)
+
+            print(former_pos_x, former_pos_y, '->', self.pos_x, self.pos_y)
+
+            if not exchange_tiles(self.image, former_pos_x, former_pos_y, self.pos_x, self.pos_y, self.patch_size):
+                pos_x = former_pos_x
+                pos_y = former_pos_y
+
+            if self.game_state == -1:
+                self.game_chain = self.game_chain[:-1]
+                self.game_state = len(self.game_chain)
+            else:
+                self.game_state += 1
+
+            print(self.game_state)
+
+        return self.image
+
 
 def list_replace(lst, a, b):
     for i in range(len(lst)):
         if lst[i] == a:
             lst[i] = b
 
-def game_loop(image, pos_x, pos_y):
-    global game_state, game_chain
-
-    if game_state < len(game_chain):
-        if game_state < 0:
-            direction = game_chain[-1]
-        else:
-            direction = game_chain[game_state]
-
-        print(direction)
-
-        former_pos_x = pos_x
-        former_pos_y = pos_y
-
-        pos_x, pos_y = new_pos(pos_x, pos_y, direction)
-
-        print(former_pos_x, former_pos_y, '->', pos_x, pos_y)
-
-        if not exchange_tiles(image, former_pos_x, former_pos_y, pos_x, pos_y):
-            pos_x = former_pos_x
-            pos_y = former_pos_y
-
-        if game_state == -1:
-            game_chain = game_chain[:-1]
-            game_state = len(game_chain)
-        else:
-            game_state += 1
-
-        print(game_state)
-
-    return image, pos_x, pos_y
 
 def crop_image(image, patch_size):
     new_width = int(image.shape[1] / patch_size) * patch_size
@@ -155,7 +169,7 @@ def set_tile_to_zero(image, x, y, patch_size):
     image[y * patch_size:(y + 1) * patch_size, x * patch_size:(x + 1) * patch_size] = 0
 
 
-def exchange_tiles(image, x1, y1, x2, y2):
+def exchange_tiles(image, x1, y1, x2, y2, patch_size):
     try:
         tile1 = image[y1 * patch_size:(y1 + 1) * patch_size, x2 * patch_size:(x2 + 1) * patch_size].copy()
         tile2 = image[y2 * patch_size:(y2 + 1) * patch_size, x1 * patch_size:(x1 + 1) * patch_size].copy()
